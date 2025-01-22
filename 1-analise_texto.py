591a6d9aebbda3be037c106341b4e9f9bef27cba
Erick <erickbarretodasilva@gmail.com>
2025-01-22 11:36:08 -0300

    um teste gravando tudo em arquivo

--- 1-analise_texto.py
+++ 1-analise_texto.py
@@ -4,7 +4,9 @@ from nltk import sent_tokenize
 from nltk import word_tokenize
 from nltk.probability import FreqDist
 
-nltk.download('punkt')
+nltk.download("punkt_tab")
+
+dic = list()
 
 # 1 - Importação do Texto
 with open(os.path.join("data", "texto.txt"), "r", encoding="utf-8") as file:
@@ -16,12 +18,23 @@ sent_tokens = sent_tokenize(texto)
 print(sent_tokens)
 print(len(sent_tokens))
 
+dic.append(sent_tokens)
+
 word_tokens = word_tokenize(texto)
 print(word_tokens)
 print(len(word_tokens))
+
+dic.append(word_tokens)
 
 # 3 - Frequência de Distribuição
 fdist = FreqDist(word_tokens)
 # print(fdist)
 print(fdist.most_common(10))
 fdist.plot(10)
+
+dic.append(fdist)
+
+print(dic)
+
+with open(os.path.join("data", "result.txt"), "w", encoding="utf-8") as file:
+    file.write(str(dic))
