482696cbe549f19805bed8f78f3102fdc1a20507
Erick <erickbarretodasilva@gmail.com>
2025-01-22 18:21:56 -0300

    Utilizando Operações em Imagens II

--- 5-analisa_imagem.py
+++ 5-analisa_imagem.py
@@ -6,4 +6,21 @@ img = Image.open("data/sparta.png")
 img.show()
 
 gray_img = img.convert("L")
-gray_img.show()
+# gray_img.show()
+
+# 2 - Utilizando Operações em Imagem I
+rotated_img = img.rotate(180)
+rotated_img.show()
+
+transpose_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+transpose_img.show()
+
+resize_img_small = img.resize((300, 200))
+resize_img_small.show()
+
+resize_img_big = img.resize((1500, 1000))
+resize_img_big.show()
+
+dim = (200, 200, 250, 300)
+crop_img = img.crop(dim)
+crop_img.show()
